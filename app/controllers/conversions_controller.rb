1aabaddd091d3fdf2fad89b80a29b6586fd897a7
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-19 11:23:46 +0900

    conversions policies working

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,8 +1,23 @@
 class ConversionsController < ApplicationController
+  before_action :set_conversion, only: [:show]
+
   def create
+    @conversion = Conversion.new(conversion_params)
+    @conversion.user = current_user
     authorize @conversion
   end
 
   def show
+    authorize @conversion
+  end
+
+  private
+
+  def conversion_params
+    params.require(:conversion).permit(:url)
+  end
+
+  def set_conversion
+    @conversion = Conversion.find(params[:id])
   end
 end
