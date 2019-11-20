c7d45a48193c15a686fd45a9309f34ad7512363d
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-20 14:10:17 +0900

    language processing working. Every text tag inside the xml is seperated by a space to prevent words sticking to each other. Pragmatic segmenter takes care of the rest and fetch_sentences_service returns an array filled with gramatically correct sentences

--- app/services/fetch_sentences_service.rb
+++ app/services/fetch_sentences_service.rb
@@ -2,10 +2,16 @@ class FetchSentencesService
   def self.call_api(conversion_url)
     video_id = CGI::parse(URI(conversion_url).query)["v"].first
     url = "http://video.google.com/timedtext?lang=en&v=#{video_id}"
-    doc = Nokogiri::XML(RestClient.get(url).body).text.squish
+    doc = Nokogiri::XML(RestClient.get(url).body)
 
     raise if doc == "" # No subtitles
 
-    PragmaticSegmenter::Segmenter.new(text: doc).segment
+    final = ""
+    doc.xpath("//transcript/text").each do |e|
+      final += " #{e.text}"
+    end
+    final.gsub!("\n", ' ')
+
+    PragmaticSegmenter::Segmenter.new(text: final).segment
   end
 end
