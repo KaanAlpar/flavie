b48019296047cb2ab5a5bc87e029cb0f3f1fc2d0
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-20 13:09:31 +0900

    refactoring the fetch_sentences_service

--- app/services/fetch_sentences_service.rb
+++ app/services/fetch_sentences_service.rb
@@ -1,19 +1,11 @@
-require 'rest-client'
-
 class FetchSentencesService
   def self.call_api(conversion_url)
     video_id = CGI::parse(URI(conversion_url).query)["v"].first
     url = "http://video.google.com/timedtext?lang=en&v=#{video_id}"
-    response = RestClient.get(url)
-    doc = Nokogiri::XML(response.body).text.squish
-    if doc != ""
-      ps = PragmaticSegmenter::Segmenter.new(text: doc)
-      sentences = ps.segment
-    else
-      puts "This video does not have subtitles"
-      raise
-    end
+    doc = Nokogiri::XML(RestClient.get(url).body).text.squish
 
-    sentences
+    raise if doc == "" # No subtitles
+
+    PragmaticSegmenter::Segmenter.new(text: doc).segment
   end
 end
