88e6d5a7168d71c52ea69e69277aa722807a4ee0
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-21 10:20:41 +0900

    FetchVideoInfoService gets the video title from youtube api

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -2,12 +2,13 @@ class ConversionsController < ApplicationController
   before_action :set_conversion, only: [:show, :choose_deck]
 
   def create
-    url = conversion_params[:url]
+    url = conversion_params[:video_id]
     video_id = CGI::parse(URI(url).query)["v"].first
+    video_info = FetchVideoInfoService.call_api(video_id)
 
     begin
-      @conversion = Conversion.find_or_create_by(url: video_id, user: current_user) do |conversion|
-        sentences = FetchSentencesService.call_api(url)
+      @conversion = Conversion.find_or_create_by(video_id: video_id, user: current_user, video_title: video_info[:title]) do |conversion|
+        sentences = FetchSentencesService.call_api(url, video_id)
         params_new = { conversion: {
           sentences_attributes: sentences.map { |sentence| { content: sentence } }
         } }
@@ -16,7 +17,7 @@ class ConversionsController < ApplicationController
       end
     rescue Conversion::MissingSubtitlesError
       @conversion = Conversion.new
-      @conversion.errors.add(:url, 'is bad. No subtitles avaliable for this video. Please try a different one.')
+      @conversion.errors.add(:video_id, 'is bad. No subtitles avaliable for this video. Please try a different one.')
     end
 
     authorize @conversion
@@ -41,7 +42,7 @@ class ConversionsController < ApplicationController
   private
 
   def conversion_params
-    params.require(:conversion).permit(:url)
+    params.require(:conversion).permit(:video_id)
   end
 
   def set_conversion
--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -2,7 +2,7 @@ class Conversion < ApplicationRecord
   class MissingSubtitlesError < StandardError; end
   belongs_to :user
   has_many :sentences, dependent: :destroy
-  validates :url, presence: true
+  validates :video_id, presence: true
   validates :user, presence: true
   accepts_nested_attributes_for :sentences
 end
