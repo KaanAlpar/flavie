1c22e182608e1eaed81b74348e5d4ce916b2926c
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-21 12:00:49 +0900

    choose deck page working using session istead of params

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,18 +1,21 @@
 class DecksController < ApplicationController
   before_action :set_deck, only: [:show, :practice]
+  skip_after_action :verify_authorized, only: [:choose_deck]
 
   def index
     @decks = policy_scope(Deck).where(user: current_user)
+    @deck = Deck.new
   end
 
   def show
     authorize @deck
-    return unless params[:deck][:flashcards_attributes]
 
-    params = deck_params.to_hash
-    params["flashcards_attributes"].each do |arr|
-      fc = Flashcard.new(sentence: Sentence.find(arr.second["sentence_id"]), deck: @deck)
-      raise unless fc.save
+    sentence_ids = session[:sentence_ids]
+    unless sentence_ids.nil?
+      sentence_ids.each do |sentence_id|
+        Flashcard.create!(sentence_id: sentence_id, deck: @deck)
+      end
+      session.delete(:sentence_ids)
     end
   end
 
@@ -22,10 +25,14 @@ class DecksController < ApplicationController
   end
 
   def create
-    @deck = Deck.new(deck_params)
-    @deck.user = current_user
+    new_params = deck_params
+    unless session[:sentence_ids].nil?
+      new_params = deck_params.merge(flashcards_attributes: session[:sentence_ids].map { |sentence_id| { sentence_id: sentence_id } })
+    end
+    @deck = Deck.new(new_params.merge(user: current_user))
     authorize @deck
     if @deck.save
+      session.delete(:sentence_ids)
       redirect_to deck_path(@deck)
     else
       render :new
@@ -36,6 +43,11 @@ class DecksController < ApplicationController
     authorize @deck
   end
 
+  def choose_deck
+    session[:sentence_ids] = params[:sentence_ids]
+    redirect_to decks_path
+  end
+
   private
 
   def set_deck
@@ -43,6 +55,6 @@ class DecksController < ApplicationController
   end
 
   def deck_params
-    params.require(:deck).permit(:name, flashcards_attributes: [:sentence_id])
+    params.require(:deck).permit(:name)
   end
 end
