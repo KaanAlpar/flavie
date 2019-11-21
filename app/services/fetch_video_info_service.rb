33aff23bf17ab0f155448712e4c768feec85479b
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-21 19:24:31 +0900

    practice page and translation service

--- app/services/fetch_video_info_service.rb
+++ app/services/fetch_video_info_service.rb
@@ -1,7 +1,7 @@
 class FetchVideoInfoService
   def self.call_api(id)
     url = "https://www.googleapis.com/youtube/v3/videos?id=#{id}&key=#{ENV['GOOGLE_API_KEY']}&part=snippet,contentDetails,statistics,status"
-    info = JSON.parse(RestClient.get(url))
+    info = JSON.parse(RestClient.get(url).body)
     title = info["items"].first["snippet"]["title"]
     channel_title = info["items"].first["snippet"]["channelTitle"]
     { info: info, title: title, id: id, channel_title: channel_title }
