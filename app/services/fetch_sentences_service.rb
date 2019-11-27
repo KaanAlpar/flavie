2ecdd3fed755bc4c85419d290cf6e8dec7a7127b
Nicole Sano <nicole.honyaku@gmail.com>
2019-11-27 19:11:21 +0900

    Japanese parsing capability

--- app/services/fetch_sentences_service.rb
+++ app/services/fetch_sentences_service.rb
@@ -1,17 +1,22 @@
 class FetchSentencesService
-  def self.call_api(video_id)
-    url = "http://video.google.com/timedtext?lang=en&v=#{video_id}"
+  def self.call_api(video_id, language)
+    url = "http://video.google.com/timedtext?lang=#{language}&v=#{video_id}"
     doc = Nokogiri::XML(RestClient.get(url).body)
 
     final = ""
     doc.xpath("//transcript/text").each do |e|
       final += " #{e.text}"
     end
-    raise Conversion::MissingSubtitlesError if final == "" # No subtitles
+    raise Conversion::MissingSubtitlesError if final == " " # No subtitles
 
     final.gsub!(/\n/, ' ')
     final.gsub!(/\(.*?\)/, '')
     final.gsub!('-- ', '')
+
+    if language == 'ja'
+      final.gsub!(' ', '.')
+      final.gsub!('　', '.')
+    end
 
     segmented = PragmaticSegmenter::Segmenter.new(text: final).segment
 
