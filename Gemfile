1ae41482abfa51314a8dd2a4d524b69543c5f2da
Nicole Sano <nicole.honyaku@gmail.com>
2019-11-27 15:02:05 +0900

    Flashcard flip JS

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'jquery-rails'
 
 gem 'iso-639'
 
--- app/javascript/controllers/flashcard_controller.js
+++ app/javascript/controllers/flashcard_controller.js
@@ -1,7 +1,9 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = ['card'];
+
   flip() {
-    this.element.classList.toggle('flashcard--flipped')
+    this.cardTarget.classList.toggle('flipped')
   }
 }
